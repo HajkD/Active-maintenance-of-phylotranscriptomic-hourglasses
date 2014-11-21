11ee01cea0b223bf0fa06a3b80ae40ab242d9e0f
Alexander Gabel <AlexGa>
2014-11-21 15:21:53 +0100

    Update createPsMap.pl

--- createPsMap.pl
+++ createPsMap.pl
@@ -1,36 +1,54 @@
 #!/usr/bin/perl
 
-
 # How to use:
 # -----------
+#
+# perl createPsMap.pl -i Athaliana_peptide.fa -d phyloBlastDB_Drost_Gabel_Grosse_Quint.fa -p BLAST_Athaliana -r athaliana_blast_results -t 30 -a 64
+#
+#
 #
 # Preparation of FASTA-Files:
 # ---------------------------
 #
-# header of FASTA file has to follow the specification:
+# header of FASTA file of the organism of interest has to follow the specification:
 #
 # >GeneID | [organism_name] | [taxonomy]
 #
 # !!! The taxonomy line begins at the nodes of Archaea or Bacteria or Eukaryota !!!
 #
-# e.g.
+# examples:
+# ---------
+#
 #
 # >NP_146894.1 | [Aeropyrum pernix] | [Archaea; Crenarchaeota; Thermoprotei; Desulfurococcales; Desulfurococcaceae; Aeropyrum]
 #
 # or
 #
-# >ATCG00500.1 | [Arabidopsis thaliana] | [Eukaryota; Viridiplantae; Streptophyta; Streptophytina; Embryophyta; Tracheophyta; Euphyllophyta; Spermatophyta; Magnoliophyta; eudicotyledons; core eudicotyledons; rosids; malvids; Brassicales; Brassicaceae; Camelineae; Arabidopsis]
+# >YP_001514406.1 | [Acaryochloris marina MBIC11017] | [Bacteria; Cyanobacteria; Oscillatoriophycideae; Chroococcales; Acaryochloris; Acaryochloris marina]
+#
+# or
 #
 # for A.thaliana
 # grep "\[Arabidopsis thaliana\]" phyloBlastDB.fa | head -1
 #
+# >ATCG00500.1|PACid:19637947 | [Arabidopsis thaliana] | [Eukaryota; Viridiplantae; Streptophyta; Streptophytina; Embryophyta; Tracheophyta; Euphyllophyta; Spermatophyta; Magnoliophyta; eudicotyledons; core eudicotyledons; rosids; malvids; Brassicales; Brassicaceae; Camelineae; Arabidopsis]
+#
 # for D.rerio
 # grep "\[Danio rerio\]" phyloBlastDB.fa | head -1
 #
+# >NP_001083027.1 | [Danio rerio] | [Eukaryota; Opisthokonta; Metazoa; Eumetazoa; Bilateria; Deuterostomia; Chordata; Craniata; Vertebrata; Gnathostomata; Teleostomi; Euteleostomi; Actinopterygii; Actinopteri; Neopterygii; Teleostei; Elopocephala; Clupeocephala; Otocephala; Ostariophysi; Otophysi; Cypriniphysi; Cypriniformes; Cyprinoidea; Cyprinidae; Danio]
+#
 # for D.melanogaster
 # grep "\[Drosophila melanogaster\]" phyloBlastDB.fa | head -1
-
-# perl createPsMap.pl -i Athaliana_peptide.fa -d phyloBlastDb.fa -p BLAST_Athaliana -r athaliana_blast_results -t 30 -a 64
+#
+# >FBpp0085360 | [Drosophila melanogaster] | [Eukaryota; Opisthokonta; Metazoa; Eumetazoa; Bilateria; Protostomia; Ecdysozoa; Panarthropoda; Arthropoda; Mandibulata; Pancrustacea; Hexapoda; Insecta; Dicondylia; Pterygota; Neoptera; Endopterygota; Diptera; Brachycera; Muscomorpha; Eremoneura; Cyclorrhapha; Schizophora; Acalyptratae; Ephydroidea; Drosophilidae; Drosophilinae; Drosophilini; Drosophilina; Drosophiliti; Drosophila; Sophophora; melanogaster group; melanogaster subgroup]
+#
+#
+# BLAST:
+# ------
+#
+# !!! To run the perl script it is necessary that BLAST and also BioPerl is already installed !!!
+#
 
 
 
@@ -38,13 +56,13 @@ use Bio::SeqIO;
 use Getopt::Long;
 
 
-my $refOrg = "Athaliana_tair10_peptide.fa";
-my $db = "BLAST_DB/phylostratigraphyBlastDb.fa";
+my $refOrg = "Athaliana_peptide.fa";
+my $db = "phyloBlastDB_Drost_Gabel_Grosse_Quint.fa";
 my $save = "BLAST_Athaliana";
 my $table = "athaliana_blast";
 my $threshold = 30;
 my $threads = 1;
-
+my $evalue = 1e-5;
 
 GetOptions (
 "input=s" => \$refOrg,
@@ -55,7 +73,6 @@ GetOptions (
 "a=i" => \$threads,
 "evalue=s" => \$evalue
 );
-
 
 my @blastFiles = glob $db."*.phr";
 
@@ -68,10 +85,10 @@ print "Loading ".$refOrg."...\n";
 
 # Loading Genes
 my $in = Bio::SeqIO->new(-format => "fasta",
-                         -file => $refOrg);
+-file => $refOrg);
 
 # Loading and writing sequences into an Array
-# makes it easier to restart at a specific seqeunces if
+# makes it easier to restart at a specific sequences if
 # the server crashes and the script has to be restarted
 
 my @genome = ();
@@ -88,7 +105,7 @@ print scalar(@genome)." sequences of ".$refOrg." will now be blasted against the
 #   *only AS-sequences with a minimal length of $threshold were send to BLAST       #
 #####################################################################################
 
-my $geneNumber = scalar @genome;
+my $geneNumber = scalar @genome -1;
 
 for my $geneIndex (0..$geneNumber){
     
@@ -100,7 +117,7 @@ for my $geneIndex (0..$geneNumber){
         $orgName = $1;
     }
     
-    print "\nGene ->".$gene->display_id." --- ".$geneIndex+1." of ".$geneNumber."\n";
+    print "\nGene ->".$gene->display_id." --- ".$geneIndex." of ".$geneNumber."\n";
     
     if(length($gene->seq) <= $threshold){
         open(OUT,">>".$save."_short_queries(".$threshold.").fa") or die $!;
@@ -117,15 +134,14 @@ for my $geneIndex (0..$geneNumber){
 ####################################################
 ## blastP against local sequence database         ##
 ##                                                ##
-## 1.Arg: name of database without extension      ##
-## 2.Arg: gene Bio::Seq - object                  ##
+## 1.Arg: BLAST database name                     ##
+## 2.Arg: gene (Bio::Seq)                         ##
 ## 3.Arg: mysql file name                         ##
 ## 4.Arg: mysql table name                        ##
 ## 5.Arg: name of reference organism              ##
-## 6.Arg: number of threads for BALST searchs     ##
+## 6.Arg: number of threads for BLAST             ##
 ## 7.Arg: BLAST e-value cutoff                    ##
 ####################################################
-
 sub blastP{
     
     use Bio::Tools::Run::StandAloneBlast;
@@ -141,13 +157,15 @@ sub blastP{
     my $threads = shift;
     my $e_val = shift;
     
+    my $psFile = $refOrgName;
+    $psFile =~ s//"_"/g;
+    
     my %psMap = ();
     
     my $refTreeLine = "";
     
     if($gene->description =~ /\| \[(.*)\]\s\| \[(.*)\]/){
         $refTreeLine = $2;
-        
     }
     
     my $path = `pwd`;
@@ -156,7 +174,7 @@ sub blastP{
     my $maxAlignments = 65200; # maximum number of alignments which can be handled with blastall
     
     @params = (-program  => 'blastp',
-    -database =>  $db,
+    -database =>  $path."/".$db,
     -expect => $e_val,
     -b => $maxAlignments,
     -a => $threads);
@@ -170,7 +188,9 @@ sub blastP{
         my $previousAccession = "";
         
         while( my $hit = $result->next_hit ) {
+            
             while( my $hsp = $hit->next_hsp ) {
+                
                 if(defined $hit and defined $hsp){
                     
                     
@@ -180,7 +200,7 @@ sub blastP{
                     if($hit->description =~ /\| \[(.*)\] \| \[(.*)\]/){
                         $orgName = $1;
                         $hitTreeLine = $2;
-                        
+                        print $orgName," --> ",$hitTreeLine."\n";
                     }else{
                         next;
                     }
@@ -200,49 +220,47 @@ sub blastP{
                         # PS of reference organism is length of taxonomy (beginning at cellular organism up to
                         # the organisms taxid itselfs)
                         @ps = ($refOrgName, scalar(@refOrgTree)+2);
+                        print join(" --> ",@ps),"\n";
                     }else{
                         @ps = get_PS($refTreeLine,$hitTreeLine);
                     }
-                }
-                
-                print $ps[0]."\t\t".$ps[1]."\t\t".$hit->accession."\t\t".$orgName."\n";
-                
-                
-                if(exists $psMap{$gene->display_id}){
-                    if($psMap{$gene->display_id} > $ps[1]){
+                    
+                    print $ps[0]."\t\t".$ps[1]."\t\t".$hit->accession."\t\t".$orgName."\n";
+                    
+                    
+                    if(exists $psMap{$gene->display_id}){
+                        if($psMap{$gene->display_id} > $ps[1]){
+                            $psMap{$gene->display_id} = $ps[1];
+                        }
+                    }else{
                         $psMap{$gene->display_id} = $ps[1];
                     }
-                }else{
-                    $psMap{$gene->display_id} = $ps[1];
+                    
+                    open(OUT,">>".$path."/".$save.".sql") or die $!;
+                    my $sql = 	"INSERT INTO ".$table." ".
+                    "VALUES(".
+                    "\'".$ps[0]."\',". # PS name
+                    "\'".$ps[1]."\',". # PS number
+                    "\'".$orgName."\',". # hit organism
+                    "\'".$gene->display_id."\',". # query id
+                    "\'".$hit->accession."\',". # hit id
+                    "\'".$hsp->length('query')."\',".
+                    "\'".$hsp->length('hit')."\',".
+                    "\'".$hsp->percent_identity."\',".
+                    "\'".$hsp->evalue."\',".
+                    "\'".$hsp->significance."\',".
+                    "\'".$hsp->score."\',".
+                    "\'".$hsp->frac_identical('query')."\',".
+                    "\'".$hsp->frac_identical('hit')."\');";
+                    print OUT $sql."\n";
+                    close(OUT);
                 }
-                
-                open(OUT,">>".$path."/".$save.".sql") or die $!;
-                my $sql = 	"INSERT INTO ".$table." ".
-                "VALUES(".
-                "\'".$ps[0]."\',". # PS name
-                "\'".$ps[1]."\',". # PS number
-                "\'".$orgName."\',". # hit organism
-                "\'".$gene->display_id."\',". # query id
-                "\'".$hit->accession."\',". # hit id
-                "\'".$hsp->length('query')."\',".
-                "\'".$hsp->length('hit')."\',".
-                "\'".$hsp->percent_identity."\',".
-                "\'".$hsp->evalue."\',".
-                "\'".$hsp->significance."\',".
-                "\'".$hsp->score."\',".
-                "\'".$hsp->frac_identical('query')."\',".
-                "\'".$hsp->frac_identical('hit')."\');";
-                print OUT $sql."\n";
-                close(OUT);
             }
         }
     }
     
-    my $psFile = $refOrgName;
-    $psFile =~ s//"_"/g;
-
-    open(OUT,">".path."/".$psFile."_psMap.csv") or die $!;
-        print OUT "ID;PS";
+    open(OUT,">".$path."/".$psFile."_psMap.csv") or die $!;
+    print OUT "ID;PS";
     while(my($k,$v) = each %psMap){
         print OUT $k.";".$v."\n";
     }
@@ -264,6 +282,7 @@ sub get_PS{
     
     # comparing taxonomy begins at the root (e.g. at node Archaea or Bacteria or Eukaryota)
     for(my $i=0, my $j=0;$i<scalar(@refLineage) && $j<scalar(@hitLineage);$i++,$j++){
+        #print "i: ".$i."   ".$taxNodes[$i]."  <--> ".$hitLineage[$j]."\n";
         if($refLineage[$i] ne $hitLineage[$j]){
             if($i == 0 && $j == 0){
                 $ps[0] = "Cellular organisms";
